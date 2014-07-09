21cc5111838412ad41bf7def651b47c815fb9314
Hru Games <imbaone@gmail.com>
2014-07-08 22:14:07 -0700

    Adding orbit control and stats

--- js/main.js
+++ js/main.js
@@ -2,14 +2,34 @@ Main = function() {
   // Hello there
 };
 
+
+Main.prototype.run = function() {
+  this.init();
+  this.animate();
+};
+
 Main.prototype.init = function() {
-  this.renderer = new THREE.WebGLRenderer();
-  this.renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( this.renderer.domElement );
+  this.clock = new THREE.Clock();
   
+  this.container = document.createElement('div');
+  document.body.appendChild(this.container);
+  
+  this.renderer = new THREE.WebGLRenderer();
+  this.renderer.setSize(window.innerWidth, window.innerHeight);
+  this.container.appendChild(this.renderer.domElement);
   
   this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
   this.camera.position.z = 400;
+  
+  // Controls
+  
+  var controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+  controls.target.set(0, 0, 0);
+  controls.noZoom = true;
+  controls.noPan = true;
+  controls.autoRotate = true;
+  
+  this.controls = controls;
   
   this.scene = new THREE.Scene();
   
@@ -23,29 +43,40 @@ Main.prototype.init = function() {
   this.mesh = new THREE.Mesh(geometry, material);
   this.scene.add(this.mesh);
 
-  this.animate();
+  this.stats = new Stats();
+  this.stats.domElement.style.position = 'absolute';
+  this.stats.domElement.style.top = '0px';
+  this.container.appendChild(this.stats.domElement);
 
   window.addEventListener('resize', $.bind(this.onWindowResize, this), false);
 };
 
-  
 Main.prototype.onWindowResize = function() {
   this.camera.aspect = window.innerWidth / window.innerHeight;
   this.camera.updateProjectionMatrix();
     
   this.renderer.setSize( window.innerWidth, window.innerHeight );
 };
-  
+
 Main.prototype.animate = function() {
   requestAnimationFrame($.bind(this.animate, this));
-  
-  this.mesh.rotation.x += 0.005;
-  this.mesh.rotation.y += 0.01;
-  
+
+  this.update(this.clock.getDelta());
+  this.render(this.clock.getDelta());
+};
+
+Main.prototype.update = function(dt) {
+  this.mesh.rotation.x += 5 / 16 * dt;
+  this.mesh.rotation.y += 10 / 16 * dt;
+
+  this.stats.update();
+};
+
+Main.prototype.render = function(dt) {
   this.renderer.render(this.scene, this.camera);
 };
-  
+
 (function() {
   var m = new Main();
-  m.init();
+  m.run();
 })();
