e2b1574fb5b27e067a680f841752114cc7c5fe34
Hru Games <imbaone@gmail.com>
2014-07-20 15:16:14 -0700

    Split Geo into multiple auto-generating boxes

--- js/world.js
+++ js/world.js
@@ -1,7 +1,18 @@
 World = function(camera) {
   this.cells_ = new Uint8Array(World.TOTAL_SIZE);
-  this.geoFaces_ = new Int32Array(World.TOTAL_SIZE * 6);
-  this.facePool_ = [];
+
+  this.boxes_ = [];
+  for (var i = 0; i < World.X_BOX_COUNT; ++i) {
+    var ys = [];
+    for (var j = 0; j < World.Y_BOX_COUNT; ++j) {
+      var zs = [];
+      for (var k = 0; k < World.Z_BOX_COUNT; ++k) {
+        zs.push(null);
+      }
+      ys.push(zs);
+    }
+    this.boxes_.push(ys);
+  }
 
   this.root_ = new THREE.Object3D();
   this.root_.position.x = -World.X_SIZE / 2;
@@ -10,23 +21,38 @@ World = function(camera) {
   this.camera_ = camera;
 };
 
-World.X_SIZE = 30;
+// World
+World.X_SIZE = 70;
 World.Y_SIZE = 30;
-World.Z_SIZE = 30;
-
-World.VERTEX_OFFSET_X = 1;
-World.VERTEX_OFFSET_Y = World.X_SIZE + 1;
-World.VERTEX_OFFSET_Z = (World.X_SIZE + 1) * (World.Y_SIZE + 1);
-
+World.Z_SIZE = 70;
 World.TOTAL_SIZE = World.X_SIZE * World.Y_SIZE * World.Z_SIZE;
 
 World.Y_OFFSET = World.X_SIZE;
 World.Z_OFFSET = World.X_SIZE * World.Y_SIZE;
 
+// Box
+World.X_BOX_SIZE = 10;
+World.Y_BOX_SIZE = 10;
+World.Z_BOX_SIZE = 10;
+World.BOX_TOTAL_SIZE = World.X_BOX_SIZE * World.Y_BOX_SIZE * World.Z_BOX_SIZE;
+
+World.X_BOX_COUNT = Math.floor((World.X_SIZE + World.X_BOX_SIZE - 1)  / World.X_BOX_SIZE);
+World.Y_BOX_COUNT = Math.floor((World.Y_SIZE + World.Y_BOX_SIZE - 1)  / World.Y_BOX_SIZE);
+World.Z_BOX_COUNT = Math.floor((World.Z_SIZE + World.Z_BOX_SIZE - 1)  / World.Z_BOX_SIZE);
+
+World.Y_BOX_OFFSET = World.X_BOX_SIZE;
+World.Z_BOX_OFFSET = World.X_BOX_SIZE * World.Y_BOX_SIZE;
+
+World.VERTEX_OFFSET_X = 1;
+World.VERTEX_OFFSET_Y = World.X_BOX_SIZE + 1;
+World.VERTEX_OFFSET_Z = (World.X_BOX_SIZE + 1) * (World.Y_BOX_SIZE + 1);
+
+World.MAX_FACE_COUNT = World.X_BOX_SIZE * World.Z_BOX_SIZE * 6 * 4;
+World.NO_FACE = (1 << 16) - 1;
+
+// Textures
 World.CELL_TX_SIZE = 1 / 4;
 World.CELL_TY_SIZE = 1 / 2;
-
-World.MAX_FACE_COUNT = World.X_SIZE * World.Z_SIZE * 6 * 4;
 
 World.CellType = {
   EMPTY: 0,
@@ -97,11 +123,31 @@ World.id_ = function(x, y, z) {
   return x + y * World.Y_OFFSET + z * World.Z_OFFSET;
 };
 
+World.localId_ = function(id) {
+  var xyz = World.localXYZ_(id);
+  return xyz[0] + xyz[1] * World.Y_BOX_OFFSET + xyz[2] * World.Z_BOX_OFFSET;
+};
+
+World.xyz_ = function(id) {
+  var z = Math.floor(id / World.Z_OFFSET);
+  id -= z * World.Z_OFFSET;
+  var y = Math.floor(id / World.Y_OFFSET);
+  id -= y * World.Y_OFFSET;
+  return [id, y, z];
+};
+
 World.isIn = function(x, y, z) {
   return x >= 0 && y >= 0 && z >= 0 &&
       x < World.X_SIZE && y < World.Y_SIZE && z < World.Z_SIZE;
 };
 
+World.localXYZ_ = function(id) {
+  var xyz = World.xyz_(id);
+  xyz[0] %= World.X_BOX_SIZE;
+  xyz[1] %= World.Y_BOX_SIZE;
+  xyz[2] %= World.Z_BOX_SIZE;
+  return xyz;
+}
 
 /**
  * @private
@@ -213,29 +259,55 @@ World.prototype.createRandomWorld_ = function() {
   }
 };
 
-/**
- * @private
- */
-World.prototype.buildMesh_ = function() {
-  var geo = new THREE.Geometry();
-  var vi = 0;
-  for (var i = 0; i < World.TOTAL_SIZE * 6; ++i) {
-    this.geoFaces_[i] = -1;
+World.prototype.getBox_ = function(x, y, z) {
+  if (!World.isIn(x, y, z)) {
+    // log error
+    return null;
   }
+  x = Math.floor(x / World.X_BOX_SIZE);
+  y = Math.floor(y / World.Y_BOX_SIZE);
+  z = Math.floor(z / World.Z_BOX_SIZE);
+  if (!this.boxes_[x][y][z]) {
+    return this.createNewGeo_(x, y, z);
+  }
+  return this.boxes_[x][y][z];
+};
+
+
+World.prototype.getBoxById_ = function(id) {
+  var xyz = World.xyz_(id);
+  return this.getBox_(xyz[0], xyz[1], xyz[2]);
+};
+
+
+World.prototype.createNewGeo_ = function(x, y, z) {
+  var box = {
+    geo: new THREE.Geometry(),
+    faces: new Uint16Array(World.BOX_TOTAL_SIZE * 6),
+    facePool: [],
+    x: x,
+    y: y,
+    z: z
+  };
+
+  for (var i = 0; i < World.BOX_TOTAL_SIZE * 6; ++i) {
+    box.faces[i] = World.NO_FACE;
+  }
+
   // Generating vertices
-  for (var k = 0; k <= World.Z_SIZE; ++k) {
-    for (var j = 0; j <= World.Y_SIZE; ++j) {
-      for (var i = 0; i <= World.X_SIZE; ++i) {
-        geo.vertices.push(new THREE.Vector3(i, j, k));
+  for (var k = 0; k <= World.Z_BOX_SIZE; ++k) {
+    for (var j = 0; j <= World.Y_BOX_SIZE; ++j) {
+      for (var i = 0; i <= World.X_BOX_SIZE; ++i) {
+        box.geo.vertices.push(new THREE.Vector3(i, j, k));
       }
     }
   }
   // Generating faces
   for (var i = 0; i < World.MAX_FACE_COUNT; ++i) {
-    this.facePool_.push(geo.faces.length);
+    box.facePool.push(box.geo.faces.length);
     for (var j = 0; j < 2; ++j) {
-      geo.faces.push(new THREE.Face3(0, 0, 0));
-      geo.faceVertexUvs[0].push([
+      box.geo.faces.push(new THREE.Face3(0, 0, 0));
+      box.geo.faceVertexUvs[0].push([
         new THREE.Vector2(0, 0),
         new THREE.Vector2(0, 0),
         new THREE.Vector2(0, 0)
@@ -243,6 +315,23 @@ World.prototype.buildMesh_ = function() {
     }
   }
 
+  var materials = [
+    Materials.getMaterial(Materials.SOLID_CELLS),
+    Materials.getMaterial(Materials.TRANSPARENT_CELLS)
+  ];
+  var mesh = new THREE.Mesh(box.geo, new THREE.MeshFaceMaterial(materials));
+  mesh.position.set(x * World.X_BOX_SIZE, y * World.Y_BOX_SIZE, z * World.Z_BOX_SIZE);
+  box.geo.computeBoundingBox();
+  box.geo.computeBoundingSphere();
+  this.root_.add(mesh);
+  this.boxes_[x][y][z] = box;
+  return box;
+};
+
+/**
+ * @private
+ */
+World.prototype.buildMesh_ = function() {
   for (var i = 0; i < World.TOTAL_SIZE; ++i) {
     var cellType = this.cells_[i];
     if (!cellType || cellType == World.CellType.EMPTY) {
@@ -252,31 +341,26 @@ World.prototype.buildMesh_ = function() {
     for (var j = 0; j < 6; ++j) {
       var nextCell = this.getNextCell_(i, j);
       if (nextCell != cellType && World.isTransparent_(nextCell)) {
-        this.addFace_(geo, i, j, cellType);
+        this.addFace_(i, j, cellType);
       }
     }
   }
-  // geo.computeVertexNormals();
-  geo.computeBoundingBox();
-  geo.computeBoundingSphere();
-  var materials = [
-    Materials.getMaterial(Materials.SOLID_CELLS),
-    Materials.getMaterial(Materials.TRANSPARENT_CELLS)
-  ];
-  this.mainMesh_ = new THREE.Mesh(geo, new THREE.MeshFaceMaterial(materials));
-  this.root_.add(this.mainMesh_);
 };
 
-World.prototype.addFace_ = function(geo, id, j, cellType) {
-  if (this.geoFaces_[id * 6 + j] >= 0) {
+World.prototype.addFace_ = function(id, j, cellType) {
+  var box = this.getBoxById_(id);
+  var localId = World.localId_(id);
+
+  if (box.faces[localId * 6 + j] < World.NO_FACE) {
     debugger;
+    return;
   }
-  var faceId = this.facePool_.pop();
-  this.geoFaces_[id * 6 + j] = faceId;
-  this.updateFace_(geo, id, j, faceId, cellType);
+  var faceId = box.facePool.pop();
+  box.faces[localId * 6 + j] = faceId;
+  this.updateFace_(box, World.localXYZ_(id), j, faceId, cellType);
 };
 
-World.prototype.updateFace_ = function(geo, id, j, faceId, cellType) {
+World.prototype.updateFace_ = function(box, xyz, j, faceId, cellType) {
   var eps = World.CELL_TX_SIZE * 0.05;
   var tx = World.CELL_TX_OFFSET[cellType] + eps;
   var ty = World.CELL_TY_OFFSET[cellType] + eps;
@@ -285,43 +369,60 @@ World.prototype.updateFace_ = function(geo, id, j, faceId, cellType) {
   var f = World.FACES[j];
   var norm = World.NEXT_OFFSET[j];
 
-  var vi = World.idToVertexRoot_(id);
-  var face = geo.faces[faceId];
+  var vi = xyz[0] * World.VERTEX_OFFSET_X +
+      xyz[1] * World.VERTEX_OFFSET_Y +
+      xyz[2] * World.VERTEX_OFFSET_Z;
+
+  var face = box.geo.faces[faceId];
   face.a = vi+World.FACE_NODES[f[0]];
   face.b = vi+World.FACE_NODES[f[1]];
   face.c = vi+World.FACE_NODES[f[2]];
   face.normal.set(norm[0], norm[1], norm[2]);
   face.materialIndex = materialIndex;
 
-  face = geo.faces[faceId + 1];
+  face = box.geo.faces[faceId + 1];
   face.a = vi+World.FACE_NODES[f[0]];
   face.b = vi+World.FACE_NODES[f[3]];
   face.c = vi+World.FACE_NODES[f[1]];
   face.normal.set(norm[0], norm[1], norm[2]);
   face.materialIndex = materialIndex;
 
-  geo.faceVertexUvs[0][faceId][0].set(tx, ty);
-  geo.faceVertexUvs[0][faceId][1].set(tx + World.CELL_TX_SIZE - 2 * eps, ty + World.CELL_TY_SIZE - 2 * eps);
-  geo.faceVertexUvs[0][faceId][2].set(tx + World.CELL_TX_SIZE - 2 * eps, ty);
-  geo.faceVertexUvs[0][faceId+1][0].set(tx, ty);
-  geo.faceVertexUvs[0][faceId+1][1].set(tx, ty + World.CELL_TY_SIZE - 2 * eps);
-  geo.faceVertexUvs[0][faceId+1][2].set(tx + World.CELL_TX_SIZE - 2 * eps, ty + World.CELL_TY_SIZE - 2 * eps);
+  box.geo.faceVertexUvs[0][faceId][0].set(tx, ty);
+  box.geo.faceVertexUvs[0][faceId][1].set(tx + World.CELL_TX_SIZE - 2 * eps, ty + World.CELL_TY_SIZE - 2 * eps);
+  box.geo.faceVertexUvs[0][faceId][2].set(tx + World.CELL_TX_SIZE - 2 * eps, ty);
+  box.geo.faceVertexUvs[0][faceId+1][0].set(tx, ty);
+  box.geo.faceVertexUvs[0][faceId+1][1].set(tx, ty + World.CELL_TY_SIZE - 2 * eps);
+  box.geo.faceVertexUvs[0][faceId+1][2].set(tx + World.CELL_TX_SIZE - 2 * eps, ty + World.CELL_TY_SIZE - 2 * eps);
+
+  box.geo.elementsNeedUpdate = true;
+  box.geo.verticesNeedUpdate = true;
+  box.geo.normalsNeedUpdate = true;
+  box.geo.uvsNeedUpdate = true;
 };
 
-World.prototype.safeRemoveFace_ = function(geo, id, j) {
-  var faceId = this.geoFaces_[id * 6 + j];
-  if (faceId >= 0) {
-    geo.faces[faceId].a = 0;
-    geo.faces[faceId].b = 0;
-    geo.faces[faceId].c = 0;
-    geo.faces[faceId + 1].a = 0;
-    geo.faces[faceId + 1].b = 0;
-    geo.faces[faceId + 1].c = 0;
-    this.facePool_.push(faceId);
-    this.geoFaces_[id * 6 + j] = -1;
-    return true;
+World.prototype.safeRemoveFace_ = function(id, j) {
+  var box = this.getBoxById_(id);
+  var localId = World.localId_(id);
+
+  var faceId = box.faces[localId * 6 + j];
+  if (faceId == World.NO_FACE) {
+    return;
   }
-  return false;
+
+  box.geo.faces[faceId].a = 0;
+  box.geo.faces[faceId].b = 0;
+  box.geo.faces[faceId].c = 0;
+  box.geo.faces[faceId + 1].a = 0;
+  box.geo.faces[faceId + 1].b = 0;
+  box.geo.faces[faceId + 1].c = 0;
+
+  box.geo.elementsNeedUpdate = true;
+  box.geo.verticesNeedUpdate = true;
+  box.geo.normalsNeedUpdate = true;
+  box.geo.uvsNeedUpdate = true;
+
+  box.facePool.push(faceId);
+  box.faces[localId * 6 + j] = -1;
 };
 
 /**
@@ -342,8 +443,6 @@ World.prototype.updateCell = function(x, y, z, newCell) {
   if (newCell == cellType) {
     return;
   }
-  var geo = this.mainMesh_.geometry;
-  var updated = false;
   for (var j = 0; j < 6; ++j) {
     var norm = World.NEXT_OFFSET[j];
     var nx = x + norm[0];
@@ -353,50 +452,23 @@ World.prototype.updateCell = function(x, y, z, newCell) {
     var nextCell = this.getCell(nx, ny, nz);
     // need to remove this face
     if (cellType != World.CellType.EMPTY && nextCell != cellType && World.isTransparent_(nextCell)) {
-      if (this.safeRemoveFace_(geo, id, j)) {
-        updated = true;
-      }
+      this.safeRemoveFace_(id, j)
     }
     if (newCell != World.CellType.EMPTY && nextCell != newCell && World.isTransparent_(nextCell)) {
-      this.addFace_(geo, id, j, newCell);
-      updated = true;
+      this.addFace_(id, j, newCell);
     }
     // near cell faces
     var nj = (j + 3) % 6;
     if (nextCell != World.CellType.EMPTY && nextCell != cellType && World.isTransparent_(cellType) &&
         (nextCell == newCell || !World.isTransparent_(newCell))) {
-      if (this.safeRemoveFace_(geo, nextId, nj)) {
-        updated = true;
-      }
+      this.safeRemoveFace_(nextId, nj);
     }
     if (nextCell != World.CellType.EMPTY && nextCell != newCell && World.isTransparent_(newCell) &&
         !(nextCell != cellType && World.isTransparent_(cellType))) {
-      this.addFace_(geo, nextId, nj, nextCell);
-      updated = true;
+      this.addFace_(nextId, nj, nextCell);
     }
   }
   this.cells_[id] = newCell;
-  if (updated) {
-    geo.elementsNeedUpdate = true;
-    geo.verticesNeedUpdate = true;
-    geo.normalsNeedUpdate = true;
-    geo.uvsNeedUpdate = true;
-    // geo.computeVertexNormals();
-//    freshGeo.computeBoundingBox();
-//    freshGeo.computeBoundingSphere();
-  }
-};
-
-
-World.idToVertexRoot_ = function(id) {
-  var z = Math.floor(id / World.Z_OFFSET);
-  id -= z * World.Z_OFFSET;
-  var y = Math.floor(id / World.Y_OFFSET);
-  id -= y * World.Y_OFFSET;
-  var x = id;
-  return x * World.VERTEX_OFFSET_X +
-      y * World.VERTEX_OFFSET_Y +
-      z * World.VERTEX_OFFSET_Z;
 };
 
 
@@ -408,16 +480,12 @@ World.prototype.getCell = function(x, y, z) {
 };
 
 World.prototype.getNextCell_ = function(id, j) {
-  var z = Math.floor(id / World.Z_OFFSET);
-  id -= z * World.Z_OFFSET;
-  var y = Math.floor(id / World.Y_OFFSET);
-  id -= y * World.Y_OFFSET;
-  var x = id;
+  var xyz = World.xyz_(id);
   var norm = World.NEXT_OFFSET[j];
   return this.getCell(
-    x + norm[0],
-    y + norm[1],
-    z + norm[2]
+    xyz[0] + norm[0],
+    xyz[1] + norm[1],
+    xyz[2] + norm[2]
   );
 };
 
@@ -441,9 +509,10 @@ World.prototype.setCell = function(x, y, z, cell) {
 };
 
 World.prototype.update = function(dt) {
-//  if (Math.random() > 0.5) {
-//    return;
-//  } 
+  if (Math.random() > 0.05) {
+    return;
+  } 
+/*
 //  for (var k = World.Y_SIZE - 1; k >= 0; --k) {
   for (var k = 0; k < World.Y_SIZE; ++k) {
     for (var i = 0; i < World.X_SIZE; ++i) {
@@ -455,10 +524,11 @@ World.prototype.update = function(dt) {
       }
     }
   }
-/*
+  */
+
   var i = Math.floor(Math.random() * World.X_SIZE);
   var k = Math.floor(Math.random() * World.Y_SIZE);
   var j = Math.floor(Math.random() * World.Z_SIZE);
   this.updateCell(i, k, j, World.CellType.ROCK);
-*/
+
 };
